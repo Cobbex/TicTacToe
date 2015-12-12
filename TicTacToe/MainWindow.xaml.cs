e5434865ee042fcf880ed041e9b068ec62f9ffd2
Cosmos <ezio.lundell@hotmail.com>
2015-12-12 02:39:17 +0100

    Everything else completed, AI almost done to. Currently looking into the most code efficient method.

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -21,115 +21,174 @@ namespace TicTacToe
     public partial class MainWindow : Window
     {
         bool gameOver;
-        enum turn {X,O};
-        turn Turn = turn.X;
         int turnCount;
         bool player1Turn = true;
+        bool computerOpponent = false;
+
+        // More logical setup, although not working correctly =(
         //private int[,] Solutions = new int[,]
         //{
         //    // Bottom
-        //    {0,1,2},
+        //    {1,3,3},
+
         //    // Middle - Horizontal
-        //    {3,4,5},
+        //    {4,5,6},
+
         //    // Top
-        //    {6,7,8},
+        //    {7,8,9},
+
         //    // Left
-        //    {0,3,6},
+        //    {1,4,7},
+
         //    // Right
-        //    {2,5,8},
+        //    {3,6,9},
+
         //    // Diagonal - Right Up
-        //    {0,4,8},
+        //    {1,5,9},
+
         //    // Diagonal - Right Down
-        //    {6,4,2},
+        //    {7,5,3},
         //};
+
+        // Required for the clicking function to work
+        // It's looking for a '0' value in the Solutions and we don't have that
+        // And we can't increase the '0' to '1' because then the problem will happen in the other end =(
         private int[,] Solutions = new int[,]
         {
-            {0,1,2},
-            {3,4,5},
-            {6,7,8},
-            {0,3,6},
-            {1,4,7},
-            {2,5,8},
-            {0,4,8},
-            {2,4,6}
+            {0,1,2},// Bottom
+            {3,4,5},// Middle - Horizontal
+            {6,7,8},// Top
+            {0,3,6},// Left
+            {1,4,7},// Middle - Vertical
+            {2,5,8},// Right
+            {0,4,8},// Diagonal - Up Right or Down Left
+            {2,4,6}// Diagonal - Up Left or Down Right
         };
-        private Button[] buttons = new Button[9];
-        int scorePlayer, scorePC;
+        private Button[] buttons = new Button[9]; // Get all buttons
+        int scorePlayer, scorePC; // Keep score
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // Feeding array with buttons
             buttons = new Button[9] { _1, _2, _3, _4, _5, _6, _7, _8, _9 };
+
+            // Setting events for all buttons & some other stuff
             for (int i = 0; i < 9; i++)
                 buttons[i].Click += new RoutedEventHandler(Click);
-            Start();
+            pcOption.Checked += new RoutedEventHandler(AIHandler);
+
+            Start(); // Start game! Reset stuff etc.
         }
 
         private void Start()
         {
             for (int i = 0; i < 9; i++)
             {
-                buttons[i].IsEnabled = true;
+                buttons[i].Focusable = true; // Not clickable
                 buttons[i].Content = "";
                 BrushConverter bc = new BrushConverter();
                 buttons[i].Background = (Brush)bc.ConvertFrom("#FFDDDDDD");
             }
-            Turn = turn.X;
-            gameOver = false;
+            player1Turn = !player1Turn; // The same player won't start after the new round
         }
 
         // Handeling all the buttons in one function!
         private void Click(object sender, RoutedEventArgs e)
         {
-            Button newBtn = (Button)sender;
+            Button newBtn = (Button)sender; // Setting new button as the 'sender' aka the button which called the function
 
-            if (gameOver)
-            {
-                MessageBoxResult result = MessageBox.Show("Game over... Start new game?", "Game over", MessageBoxButton.YesNo);
-                if (result == MessageBoxResult.Yes)
-                    Start();
-            }
+            //if (gameOver) // Are we finished yet?
+            //{
+            //    MessageBoxResult result = MessageBox.Show("Game over... Start new game?", "Game over", MessageBoxButton.YesNo);
+            //    if (result == MessageBoxResult.Yes)
+            //        Start();
+            //}
 
-            if (!newBtn.Content.Equals(""))
-                newBtn.IsEnabled = false;
+            if (!newBtn.Content.Equals("")) // Is it empty?
+                newBtn.Focusable = false;
 
-            if (player1Turn)
+            if (player1Turn) // Who's turn is it?
                 newBtn.Content = "X";
-            else
+            if (!player1Turn)
                 newBtn.Content = "O";
 
-            Turn = turn.O;
             player1Turn = !player1Turn;
-            turnLbl.Content = Turn;
 
-            gameOver = CheckWinner(buttons);
+            switch (computerOpponent)
+            {
+                case true:
+                    if (player1Turn)
+                        turnLbl.Content = "You";
+                    else
+                        turnLbl.Content = "PC";
+                    break;
+                case false:
+                    if (player1Turn)
+                        turnLbl.Content = "Player 1";
+                    else
+                        turnLbl.Content = "Player 2";
+                    break;
+            }
+
+            CheckWinner(buttons);
         }
 
-        //private void AI()
-        //{
-        //    if (Turn == turn.O)
-        //    {
-        //        foreach (Button btn in buttons)
-        //        {
-        //            // do stuff
-        //        }
-        //        // Player's turn
-        //        Turn = turn.X;
-        //    }
-        //}
-
-        private bool CheckWinner(Button[] buttons)
+        // Bad name, I know but it's logical
+        // Checks settings and changes game settings accordingly
+        private void AIHandler(object sender, RoutedEventHandler e)
         {
-            bool gm = false;
+            // Are we playing with a PC or PvP
+            if (pcOption.IsChecked == true)
+            {
+                AI(); // Start AI
+                computerOpponent = true;
+                Start();
+            }
+        }
+
+        private void AI()
+        {
+            if (!player1Turn)
+            {
+                if (buttons[5].Content.Equals(""))
+                    AISetButton(5);
+                if (buttons[0].Content.Equals("X") & buttons[2].Content.Equals("X"))
+                    AISetButton(1);
+            }
+        }
+
+        private void AISetButton(int index)
+        {
+            if (index > 0 && index < 8)
+            {
+                buttons[index].Content = "O";
+                buttons[index].Focusable = false;
+            }
+        }
+
+        private void WinnerMessage(bool player1Win)
+        {
+            string winner = player1Win ? "Player 1" : "Player 2";
+            MessageBoxResult result = MessageBox.Show($"{winner} won this round!\nStart new game?", "Game over!", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+                Start();
+            else
+                Application.Current.Shutdown();
+        }
+
+        private void CheckWinner(Button[] newButtons)
+        {
             for (int i = 0; i < 8; i++)
             {
                 int a = Solutions[i, 0],
                     b = Solutions[i, 1],
                     c = Solutions[i, 2];
 
-                Button bt1 = buttons[a],
-                    bt2 = buttons[b],
-                    bt3 = buttons[c];
+                Button bt1 = newButtons[a],
+                    bt2 = newButtons[b],
+                    bt3 = newButtons[c];
 
                 // If selections are "blank", continue function
                 if (bt1.Content.Equals("") || bt2.Content.Equals("") || bt3.Content.Equals(""))
@@ -145,11 +204,10 @@ namespace TicTacToe
                     bt2.FontWeight = FontWeights.Bold;
                     bt3.Background = Brushes.Red;
                     bt3.FontWeight = FontWeights.Bold;
-                    gm = true;
+                    WinnerMessage(false);
                     break;
                 }
             }
-            return gm;
         }
     }
 }
